866d85a513f39f49099dda988b99932ca5493dc4
Otis Chou <altispec@gmail.com>
2015-12-28 22:12:28 +0800

    [Feature] finish 4, 5, 6

--- spec/working_day_calculator_spec.rb
+++ spec/working_day_calculator_spec.rb
@@ -67,5 +67,35 @@ RSpec.describe WorkingDayCalculator do
         expect(cal.after_work_days_of(5)).to eq(Date.new(2015, 12, 28))
       end
     end
+
+    context 'when pass -1 day' do
+      it 'returns day before 1 workday' do
+        expect(cal.after_work_days_of(-1)).to eq(Date.new(2015, 12, 18))
+      end
+    end
+
+    context 'when pass -5 days' do
+      it 'returns day before 5 workdays' do
+        expect(cal.after_work_days_of(-5)).to eq(Date.new(2015, 12, 14))
+      end
+    end
+  end
+
+  describe '#work_day?' do
+    context 'when pass sunday' do
+      let(:cal) { WorkingDayCalculator.new(Date.new(2015, 12, 27)) }
+
+      it 'returns false' do
+        expect(cal.work_day?).to be_falsy
+      end
+    end
+
+    context 'when pass not holiday' do
+      let(:cal) { WorkingDayCalculator.new(Date.new(2015, 12, 28)) }
+
+      it 'returns true' do
+        expect(cal.work_day?).to be_truthy
+      end
+    end
   end
 end
