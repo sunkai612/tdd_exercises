29ec38c663636e0386df7eb8deeadfcc88c2a993
Otis Chou <altispec@gmail.com>
2015-12-28 22:15:15 +0800

    fix tabs

--- working_day_calculator.rb
+++ working_day_calculator.rb
@@ -1,45 +1,30 @@
 require 'Date'
 
 class WorkingDayCalculator
-	attr_reader :date, :custom_holidays
+  attr_reader :date
 
-  DEFAULT_HOLIDAYS = [
-    Date.new(2015, 1, 1),
-    Date.new(2015, 2, 8),
-    Date.new(2015, 2, 9),
-    Date.new(2015, 2, 10),
-    Date.new(2015, 2, 11),
-    Date.new(2015, 2, 12),
-    Date.new(2016, 2, 29),
-    Date.new(2015, 4, 5),
-    Date.new(2015, 6, 10),
-    Date.new(2015, 9, 16),
-    Date.new(2015, 10, 10)
-  ]
+  def initialize(date)
+    @date = date
+  end
 
-	def initialize(date, custom_holidays=[])
-		@date = date
-    @custom_holidays = custom_holidays
-	end
-
-	def next_work_day
+  def next_work_day
     result = date
 
-    while(holiday?(result)) do
+    while(holiday?(result)) do 
       result = result.next_day
     end
 
     result
-	end
+  end
 
   def prev_work_day
     result = date
 
-    while(holiday?(result)) do
+    while(holiday?(result)) do 
       result = result.prev_day
     end
 
-    result
+    result 
   end
 
   def after_work_days_of(number)
@@ -47,32 +32,44 @@ class WorkingDayCalculator
     result = date
 
     while count != 0 do
-      result = count < 0 ? result.prev_day : result.next_day
+      result = result.next_day
 
       unless holiday?(result)
-        count < 0 ? count += 1 : count -= 1
+        count -= 1
       end
     end
 
     result
   end
 
-  def work_day?
-    !holiday?(date)
+  def method_name
+    
   end
 
-	private
+  private
 
   def isWeekend?(current)
     return current.sunday? || current.saturday?
   end
 
-	def date_equal?(current, date2)
-		return current.mday == date2.mday && current.month == date2.month
-	end
+  def date_equal?(current, date2)
+    return current.mday == date2.mday && current.month == date2.month
+  end
 
   def holiday?(current)
-    holidays = DEFAULT_HOLIDAYS + custom_holidays
+    holidays = [
+      Date.new(2015, 1, 1),
+      Date.new(2015, 2, 8),
+      Date.new(2015, 2, 9),
+      Date.new(2015, 2, 10),
+      Date.new(2015, 2, 11),
+      Date.new(2015, 2, 12),
+      Date.new(2016, 2, 29),
+      Date.new(2015, 4, 5),
+      Date.new(2015, 6, 10),
+      Date.new(2015, 9, 16),
+      Date.new(2015, 10, 10)
+    ]
 
     if isWeekend?(current)
       return true
