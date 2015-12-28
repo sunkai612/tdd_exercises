2d0410f1dd99df17337ab42c7c4d9bf0fca6f84b
JuanitoFatas <katehuang0320@gmail.com>
2015-12-28 21:15:46 +0800

    Finish Q1-3

--- spec/working_day_calculator_spec.rb
+++ spec/working_day_calculator_spec.rb
@@ -1,4 +1,32 @@
-require_relative 'working_day_calculator'
+require_relative '../working_day_calculator'
 
 RSpec.describe WorkingDayCalculator do
+  describe ".previous_business_date" do
+    it "1/4, 5 => 1/11" do
+      input_date = Date.new(2016, 1, 3) # Saturday
+
+      result = WorkingDayCalculator.previous_business_date(input_date)
+
+      expect(result).to eq Date.new(2015, 12, 31)
+    end
+  end
+
+  describe ".next_business_date" do
+    it "weekend returns business monday" do
+      input_date = Date.new(2016, 1, 2) # Saturday
+
+      expect(WorkingDayCalculator.next_business_date(input_date)).to eq Date.new(2016, 1, 4)
+    end
+  end
+
+  describe ".next_business_date_by_range" do
+    it "1/4, 5 => 1/11" do
+      input_date = Date.new(2016, 1, 4) # Saturday
+      range = 5
+
+      result = WorkingDayCalculator.next_business_date_by_range(input_date, range)
+
+      expect(result).to eq Date.new(2016, 1, 11)
+    end
+  end
 end
--- working_day_calculator.rb
+++ working_day_calculator.rb
@@ -1,3 +1,59 @@
-class WorkingDayCalculator
+require "biz"
+require "biz/core_ext"
 
+Biz.configure do |config|
+  config.holidays = [
+    Date.new(2016, 1, 1),
+    Date.new(2016, 2, 8),
+    Date.new(2016, 2, 9),
+    Date.new(2016, 2, 10),
+    Date.new(2016, 2, 11),
+    Date.new(2016, 2, 12),
+    Date.new(2016, 2, 29),
+    Date.new(2016, 4, 5),
+    Date.new(2016, 6, 10),
+    Date.new(2016, 9, 16),
+    Date.new(2016, 10, 10)
+  ]
+end
+
+class WorkingDayCalculator
+  def self.previous_business_date(input_date)
+    return input_date if input_date.business_day?
+
+    current_date = input_date
+
+    while !current_date.business_day?
+      current_date = current_date.prev_day
+
+      return current_date if current_date.business_day?
+    end
+  end
+
+  def self.next_business_date(input_date)
+    return input_date if input_date.business_day?
+
+    current_date = input_date
+
+    while !current_date.business_day? # holiday
+      current_date = current_date.next
+
+      return current_date if current_date.business_day?
+    end
+  end
+
+  # 1/4, 5 => 1/11
+  def self.next_business_date_by_range(input_date, range)
+    current_date = input_date
+
+    while range != 0
+      current_date = current_date.next
+
+      if current_date.business_day?
+        range -= 1
+      end
+    end
+
+    current_date
+  end
 end
